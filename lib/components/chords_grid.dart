670b6deaf2b19075e5d49d8adf585d8d95b55dc5
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-04-28 23:43:39 -0500

    feat: Paint chord in canvas (#1)
    * Draw freet and finger positions
    
    * Draw number on nut
    
    * Update
    
    * Load chords
    
    * Update
    
    * Use json to load data
    
    * Update

---
+++ lib/components/chords_grid.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:test_drive/components/chord_widget.dart';
+import 'package:test_drive/models/chord_model.dart';
+
+class ChordsGrid extends StatelessWidget {
+  final List<ChordModel> chords;
+
+  const ChordsGrid({super.key, required this.chords});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('GridView Demo')),
+      backgroundColor: Colors.white,
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(10),
+          //physics: NeverScrollableScrollPhysics(),
+          childAspectRatio: 3 / 4,
+          // shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          children: List.generate(2, (index) {
+            print("Index: $index");
+            if (index == 0)
+              return ChordWidget(key: ValueKey(index), chord: chords[index]);
+            else {
+              final button = ElevatedButton(
+                onPressed: () {
+                  print("Button pressed");
+                },
+                child: Text("Button"),
+              );
+              return button;
+            }
+          })),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:test_drive/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: SimpleGridDemo(),
-    );
-  }
-}
-
-class SimpleGridDemo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('GridView Demo')),
-      body: GridView(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        padding: const EdgeInsets.all(10),
-        //physics: NeverScrollableScrollPhysics(),
-        shrinkWrap: false,
-        scrollDirection: Axis.vertical,
-        children: [
-          Image.network('https://picsum.photos/250?image=1'),
-          Image.network('https://picsum.photos/250?image=2'),
-          Image.network('https://picsum.photos/250?image=3'),
-          Image.network('https://picsum.photos/250?image=4'),
-          Image.network('https://picsum.photos/250?image=5'),
-          Image.network('https://picsum.photos/250?image=6'),
-          Image.network('https://picsum.photos/250?image=7'),
-          Image.network('https://picsum.photos/250?image=8'),
-          Image.network('https://picsum.photos/250?image=9'),
-          Image.network('https://picsum.photos/250?image=10'),
-          Image.network('https://picsum.photos/250?image=11'),
-          Image.network('https://picsum.photos/250?image=12'),
-          Image.network('https://picsum.photos/250?image=13'),
-          Image.network('https://picsum.photos/250?image=14'),
-          Image.network('https://picsum.photos/250?image=15'),
-          Image.network('https://picsum.photos/250?image=16'),
-          Image.network('https://picsum.photos/250?image=17'),
-          Image.network('https://picsum.photos/250?image=18'),
-          Image.network('https://picsum.photos/250?image=19'),
-        ],
-      ),
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
