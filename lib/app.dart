e31756f22b6b321965c15890db117d99467a400d
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-05-18 23:15:13 -0500

    Add keyboard actions

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:test_drive/bottom_navigation_view/bottom_bar_view.dart';
 import 'package:test_drive/models/chord_model.dart';
 import 'package:test_drive/views/search_view.dart';
@@ -61,8 +62,10 @@ class _AppState extends State<App> {
           Widget getBody() {
             switch (selectedIndex) {
               case 0:
-                return SearchView(
-                    key: const PageStorageKey('search_view'), chords: chords);
+                return KeyboardVisibilityProvider(
+                    child: SearchView(
+                        key: const PageStorageKey('search_view'),
+                        chords: chords));
               case 1:
                 return const Text('Collections');
               case 2:
---
+++ lib/components/keyboard_attachable_footer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:keyboard_attachable/keyboard_attachable.dart';
+
+class KeyboardAttachableFooter extends StatelessWidget {
+  final void Function(String) onTap;
+
+  const KeyboardAttachableFooter({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    const buttonStyle = ButtonStyle(
+      padding: MaterialStatePropertyAll(EdgeInsets.all(0)),
+      textStyle: MaterialStatePropertyAll(TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w400,
+      )),
+    );
+
+    return KeyboardAttachable(
+      child: Container(
+        padding: const EdgeInsets.only(left: 0, top: 7, bottom: 2, right: 0),
+        height: 40,
+        color: const Color(0xFFd1d3da),
+        child: Row(
+          children: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () => onTap('maj'), // Call onTap with 'maj'
+                  style: buttonStyle,
+                  child: const Text('maj'),
+                ),
+                TextButton(
+                  onPressed: () => onTap('min'), // Call onTap with 'min'
+                  style: buttonStyle,
+                  child: const Text('min'),
+                ),
+                TextButton(
+                  onPressed: () => onTap('sus'), // Call onTap with 'sus'
+                  style: buttonStyle,
+                  child: const Text('sus'),
+                ),
+                TextButton(
+                  onPressed: () => onTap('add'), // Call onTap with 'add'
+                  style: buttonStyle,
+                  child: const Text('add'),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () => onTap('#'), // Call onTap with '♯'
+                  child: const Text('♯'),
+                ),
+                TextButton(
+                  onPressed: () => onTap('b'), // Call onTap with '♭'
+                  child: const Text('♭'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
