ed15cf2eb25bc1eaaa7ee78b38b4ceaba28c8187
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-05-24 01:09:13 -0500

    Update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,10 @@ import 'package:test_drive/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App(initialThemeMode: ThemeMode.light));
+    await tester.pumpWidget(App(
+      initialThemeMode: ThemeMode.light,
+      locale: const Locale('en'),
+    ));
 
     // Verify that our counter starts at 0.
     expect(0, equals(0));
