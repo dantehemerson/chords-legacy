ca4d3333a220407da63d8cb51810371c9a734976
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-05-30 23:50:49 -0500

    Update

---
+++ lib/models/collection_model.dart
@@ -0,0 +1,11 @@
+class Collection {
+  final int id;
+  final String name;
+  final DateTime createdAt;
+
+  const Collection({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+}
